9445f6c1d980cba8fb599e2658b60a4639b435e5
Dr. Joachim Schneider <jesmx@hal.rhein-neckar.de>
2023-10-20 11:38:19 +0200

    Re-structured IsDOSPath().

--- ue500/src/unix.c
+++ ue500/src/unix.c
@@ -1502,68 +1502,50 @@ int rename P2_(char *, file1, char *, file2)
  */
 static int  IsDOSPath P1_(CONST char *, path)
 {
-    int len = 0;
+    int         len       = 0;
+    CONST char  *pFSlash  = NULL; /* Points to '/'  */
+    CONST char  *pBSlash  = NULL; /* Points to '\\' */
 
     if ( NULL == path || '\0' == *path )  {
         return 0;
     }
 
-    len = strlen(path);
-    if        ( 1 == len )  {
-        if        ( '\\' == *path ) {
-            return  ( 1 );
-        } else if ( '/' == *path )  {
-            return  ( -1 );
-        } else                      {
-            return  ( 0 );
+    ASRT(1 <= (len = strlen(path)));
+
+    if        ( '\\' == *path ) {
+        return  ( 1 );
+    } else if ( '/' == *path )  {
+        return  ( -1 );
+    } else if ( 1 == len )      {
+        return ( 0 );
+    }
+
+
+    /* 2 .GE. len .AND. *path .NOT. .IN. { '\\', '/' }  */
+
+    /* Path with drive letter, e.g. "C:": */
+    if  ( ISALPHA(*path) && ':' == *(path + 1) )  {
+        return  ( 1 );
+    }
+
+    pFSlash = strchr(path, '/');
+    pBSlash = strchr(path, '\\');
+    if ( NULL == pFSlash  ) {
+        if ( NULL == pBSlash )  {
+            return ( 0 );
+        } else                  {
+            return ( 1 );
         }
-    } else if ( 2 == len )  {
-        if        ( '\\' == *path ) {
-            return  ( 1 );
-        } else if ( '/' == *path )  {
-            return  ( -1 );
-        } else                      {
-            if        ( ISALPHA(*path) && ':' == *(path + 1) )  {
-                return  ( 1 );
-            } else if ( '.' == *path )                          {
-                if        ( '.'  == *(path + 1) ) {
-                    return ( 0 );
-                } else if ( '\\' == *(path + 1) ) {
-                    return ( 1 );
-                } else if ( '/' == *(path + 1) )  {
-                    return ( -1 );
-                } else                            {
-                    return ( 0 );
-                }
-            } else                                              {
-                return 0;
-            }
-        }
-    } else  /* 3 <= len */  {
-        if        ( '\\' == *path ) {
-            return  ( 1 );
-        } else if ( '/' == *path )  {
-            return  ( -1 );
-        } else                      {
-            if        ( ISALPHA(*path) && ':' == *(path + 1) )  {
-                return  ( 1 );
-            } else if ( '.' == *path )                          {
-                if        ( '.'  == *(path + 1) ) {
-                    if        ( '\\' == *(path + 2) ) {
-                        return ( 1 );
-                    } else if ( '/' == *(path + 2) )  {
-                        return ( -1 );
-                    } else                            {
-                        return ( 0 );
-                    }
-                } else if ( '\\' == *(path + 1) ) {
-                    return ( 1 );
-                } else if ( '/' == *(path + 1) )  {
-                    return ( -1 );
-                } else                            {
-                    return ( 0 );
-                }
-            } else                                {
+    } else                  {
+        if ( NULL == pBSlash )  {
+            return ( -1 );
+        } else                  {
+            /* '/' and '\\' occur in path:  */
+            if ( pBSlash < pFSlash )  {
+                return ( 1 );
+            } else if ( pBSlash > pFSlash ) {
+                return ( - 1 );
+            } else                          {
                 return ( 0 );
             }
         }
